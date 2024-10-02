c9081aa8f901f3d4d69305084b612ca914671c59
Arakaki, Everton <everton.arakaki@rabobank.nl>
2024-10-02 13:54:50 +0200

    add long running

--- example1.py
+++ example1.py
@@ -2,7 +2,13 @@ import datetime
 import pendulum
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import PythonOperator
 
+import time
+
+def long_running_task():
+    time.sleep(1000000000)
+    raise Exception("This task is designed to fail.")
 
 with DAG(
     dag_id="Example1",
@@ -14,8 +20,10 @@ with DAG(
     tags=["everton-arakaki-test"],
 ) as dag:
 
-    start = EmptyOperator(task_id="start")
-    task1 = EmptyOperator(task_id="task1")
+    tasks = [PythonOperator(
+        task_id=f"task{i}",
+        python_callable=long_running_task
+    ) for i in range(18)]
     end = EmptyOperator(task_id="end")
 
-start >> task1 >> end
+    [tasks] >> end
